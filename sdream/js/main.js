a66f65a839bd6088dee4feb5c86646427dca89bb
babyHamuster <chai025813@gmail.com>
2024-06-18 17:50:46 +0900

    06.18

--- sdream/js/main.js
+++ sdream/js/main.js
@@ -7,6 +7,6 @@ $(document).ready(function(){
     $('footer .top button').on('click', function(){
         $('html, body').animate({
             scrollTop : 0
-        }, 500)
+        }, 500 )
     })
 })
