26b53358654f4f718e60648dabda23ee7a687cea
babyHamuster <chai025813@gmail.com>
2024-06-18 17:47:54 +0900

    06.18

---
+++ sdream/js/main.js
@@ -0,0 +1,12 @@
+/* 
+    top 버튼을 클릭하면
+    상단으로 스크롤
+    footer .top button -- top 버튼 선택자
+*/
+$(document).ready(function(){
+    $('footer .top button').on('click', function(){
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500)
+    })
+})
