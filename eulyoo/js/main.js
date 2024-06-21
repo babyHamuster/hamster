3b090b692f066fc3beb297a7da6b3540a598cb06
babyHamuster <chai025813@gmail.com>
2024-06-21 13:18:59 +0900

    06.21

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-        const visual_swiper = new Swiper('.visual.swiper', { /* 팝업을 감싼는 요소의 class명 */
+        const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 2500,
+            delay: 3000,
             disableOnInteraction: true,
         },
+        effect: "fade", /* fade 효과 */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
         navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.visual.swiper-button-next',  /* 다음 버튼의 클래스명 */
-            prevEl: '.visual.swiper-button-prev',  
+            nextEl: '.visual .btn_prev',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .btn_next',  
         },
     });
 })//documant.ready
