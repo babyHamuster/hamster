4ee6d0ac2461e2f8aadea1f5cdcd6a57a75cdbb6
babyHamuster <chai025813@gmail.com>
2024-06-24 17:59:15 +0900

    06.24 을유

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -51,6 +51,30 @@ $(document).ready(function(){
         .interview .list ul li
         오버한 li에만 on클래스를 주고
         이전에 오버했던 li에는 on클래스를 삭제
-
+        ---> 이전에 오버했던 li를 선택하는 명령 X
+            모든 li에 있는 on 클래스를 다 삭제
+            오버한 li에만 다시 on클래스 추가
+        <ul>
+            <li></li> -- 오버, 이 li만을 지칭하는 언어 $(this)
+            <li></li>
+            <li></li>
+        </ul>
      */
-})//documant.ready
+    $('.interview .list ul li').on('mouseenter' , function(){
+        $('.interview .list ul li').removeClass('on')
+        $(this).addClass('on')
+    })
+
+    const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+        spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
+        breakpoints: {
+            640: {  /* 640px 이상이 되면 적용 */
+                slidesPerView: 4,
+                spaceBetween: 24, 
+            },
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    }); //swiper
+
+})//documant.ready  
