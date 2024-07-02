5f7e112cc321f2ae169a0765e187d82c18e7e802
babyHamuster <chai025813@gmail.com>
2024-07-02 13:06:48 +0900

    07.02 애니멀

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,29 +1,28 @@
 $(document).ready(function(){
 
-    /* 브라우저 넓이가 1024이하면 모바일 이상 pc */ 
+    /* 브라우저 넓이가 1024이하면 모바일 이상 pc */
     let pc_mobile //현재상태가 pc인지 모바일인지를 저장
     let window_w //브라우저 넓이 저장
-    
+
     function resize_chk(){
         window_w = $(window).width()
         if(window_w > 1024){ /* 1024보다 크면 - pc면 */
             pc_mobile = 'pc'
-        }else{ /* mobile 이면 */
+        }else{ /* mobile이면 */
             pc_mobile = 'mobile'
         }
         console.log(pc_mobile)
     }
     //처음에 로딩 됐을때 실행
     resize_chk()
-    //브라우저가 리사이즈 될 때마다 실행
+    //브라우저가 리사이즈 될때마다 실행
     $(window).resize(function(){
         resize_chk()
-        }
-    )
+    })
 
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 1000,
+            delay: 10000,
             disableOnInteraction: true,
         },
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */     
@@ -59,6 +58,10 @@ $(document).ready(function(){
         $('.visual .btn_wrap button.btn_stop').show()
     })//btn_play
 
+    /* 
+        페이지가 스크롤되면 header에 fixed 클래스를 추가
+        다시 맨 위로 올라가면 header에 fixed 클래스 삭제  */
+    
     let scrolling
 
     function scroll_chk(){
@@ -79,46 +82,95 @@ $(document).ready(function(){
         scroll_chk()
     })//window.scroll
 
-
-    /*
-        메뉴에 마우스를 올리면
-        header에 menu_over라는 클래스를 추가
+    /* 
+        메뉴에 마우스를 올리면  
+        header에 menu_over라는 클래스를 추가 
         마우스를 오버한 li에만 on이라는 클래스를 추가
-        메뉴 :  header .gnb .gnb_wrap ul.depth1 > li
+        메뉴 : header .gnb .gnb_wrap ul.depth1 > li
 
-        --> 다른 메뉴(li)에 마우스를 오버하면
-        이전에 오버했던 메뉴의 on클래스 삭제
-        (이전에 오버했던 메뉴를 알기 어려움)
-            ==> 모든 메뉴의 on클래스를 삭제했다가 오버한 내것만 on클래스 추가
-        --> header에서 마우스를 아웃하면 그때 menu_over라는 클래스를 삭제
+        --> 다른 메뉴(li)에 마우스를 오버하면 
+            이전에 오버했던 메뉴의 on클래스 삭제
+            (이전에 오버했던 메뉴를 알기 어려움...)
+            ==> 모든 메뉴의 on클래스를 삭제했다가 오버한 내꺼만 on클래스 추가
+        --> header에서 마우스를 아웃하면 그때 menu_over라는 클래스 삭제
             모든 메뉴에서 on클래스 삭제
-    
     */
-
+    
     $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
         if(pc_mobile == 'pc'){ //pc일 경우에만
-            $('header').addClass('menu_over')   
+            $('header').addClass('menu_over')
             $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
             $(this).addClass('on')//마우스를 오버한 li만
         }//if
     })//on
-    $('header').on('mouseleave' , function (){
+    $('header').on('mouseleave', function(){
         if(pc_mobile == 'pc'){ //pc일 경우에만
-            $('header').removeClass('menu_over')   
+            $('header').removeClass('menu_over')
             $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('on')
-         }//if
+        }//if
     })
 
-    $('header .gnb .gnb_open').on('click' , function(){
-        if(pc_mobile == 'pc'){ //pc일 경우에만
-        $('header').addClass('mobile_open')    
+    $('header .gnb .gnb_open').on('click', function(){
+        if(pc_mobile == 'mobile'){ //모바일일 경우에만
+            $('header').addClass('mobile_open')
         }
     })
-    $('header .gnb .gnb_close').on('click' , function(){
-        if(pc_mobile == 'pc'){ //pc일 경우에만
-        $('header').removeClassClass('mobile_open')    
+    $('header .gnb .gnb_close').on('click', function(){
+        if(pc_mobile == 'mobile'){ //모바일일 경우에만
+            $('header').removeClass('mobile_open')
         }
+
+
     })
+        const find01_swiper = new Swiper('.find .tab .find01 .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 2,
+                spaceBetween: 16,
+            },
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 3,
+                spaceBetween: 16,
+            },
+            1024: {   /* 1024px 이상일때 적용 */
+                slidesPerView: 4,
+                spaceBetween: 24,
+            },
+        },
+    
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.find .tab .find01 .btn_wrap .next',
+            prevEl: '.find .tab .find01 .btn_wrap .prev',
+        },
+    });//find01_swiper
+
+    const find02_swiper = new Swiper('.find .tab .find02 .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+    spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+    breakpoints: {
+        640: {    /* 640px 이상일때 적용 */
+            slidesPerView: 2,
+            spaceBetween: 16,
+        },
+        768: {    /* 768px 이상일때 적용 */
+            slidesPerView: 3,
+            spaceBetween: 16,
+        },
+        1024: {   /* 1024px 이상일때 적용 */
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    },
+
+    loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    navigation: {
+        nextEl: '.find .tab .find02 .btn_wrap .next',
+        prevEl: '.find .tab .find02 .btn_wrap .prev',
+    },
+});//find01_swiper
 
 
 })//document
