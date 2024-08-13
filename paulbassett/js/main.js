1a950d0bcbbd336f36804295de34e30a919323ad
babyHamuster <chai025813@gmail.com>
2024-08-13 13:17:27 +0900

    0813

--- paulbassett/js/main.js
+++ paulbassett/js/main.js
@@ -12,11 +12,13 @@ $(document).ready(function(){
     });
 
     $('.menu .list .popup').slick({
+      autoplay: false, //팝업 자동 실행, false)
         dots: false, //하단 페이지 버튼 (true, false)
 	    arrows: false,  //다음, 이전팝업 (true, false)
         slidesToShow: 5, //한번에 보일 팝업 수
         swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
         centerMode: true, //가운데정렬(가운데가 1번)
+        variableWidth: true, //넓이를 자유롭게 설정
         responsive: [
             {
               breakpoint: 1300, //1300px 이하
@@ -45,4 +47,74 @@ $(document).ready(function(){
         ] 
     })
 
+      $('.md .list .popup').slick({
+        autoplay: false, //팝업 자동 실행, false)
+          slidesToShow: 4, //한번에 보일 팝업 수
+          dots: false, //하단 페이지 버튼 (true, false)
+          arrows: false,  //다음, 이전팝업 (true, false)
+          swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+          variableWidth: true, //넓이를 자유롭게 설정
+          responsive: [
+            {
+              breakpoint: 1300, //1300px 이하
+              settings: {
+                slidesToShow: 4
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 375,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+        ] 
+      })
+
+      $('.sns .list .popup').slick({
+        autoplay: false, //팝업 자동 실행, false)
+          slidesToShow: 4, //한번에 보일 팝업 수
+          dots: false, //하단 페이지 버튼 (true, false)
+          arrows: false,  //다음, 이전팝업 (true, false)
+          swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+          variableWidth: true, //넓이를 자유롭게 설정
+          responsive: [
+            {
+              breakpoint: 1300, //1300px 이하
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2 ,
+                centerMode: true
+              }
+            },
+            {
+              breakpoint: 375,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+        ] 
+      })
 })//$(document).ready
